11a5348462320ff1537cf0a6272904cd29c193f6
Brandan Marcos <Marcos.Brandan@alu.utn.edu.ar>
2020-07-31 21:23:28 -0300

    Crear Archivo MAYMEN.c

---
+++ MAYMEN.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+main()
+{
+	int NumeroSecreto, NumeroIngresado, Puntaje=10;
+	int inicio=1, limite=999, i;
+	
+	srand(time(NULL));
+	NumeroSecreto = 1+rand()%(999-1+1);
+	
+	printf("\t\t\tEste juego consiste en adivinar un numero que se genera automaticamente");
+	printf("\n\n* Tiene 10 intentos *");
+	
+	printf("\n\nPor favor ingrese un numero del 1 al 999: ");
+		
+		
+		for(i=0; i<10; i++)
+		{
+			
+			
+			printf("\nIntento %d: ", i+1);
+			scanf("%d", &NumeroIngresado);
+			if(NumeroIngresado>0 && NumeroIngresado<1000)
+			{
+				if (NumeroSecreto < NumeroIngresado)
+				{
+					printf("El numero secreto es menor y se encuentra entre %d y %d", inicio, NumeroIngresado-1);
+					limite=NumeroIngresado-1;
+				}
+			}
+		}
+}
