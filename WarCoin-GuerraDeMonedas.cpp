911a2544cad7044aff7fba66390cb43dd2560d7c
Rocio Araya <arayarocio227@gmail.com>
2020-08-04 16:22:31 -0300

    Funcion y mensaje de Numero aleatorio, comienza. Agregados

--- WarCoin-GuerraDeMonedas.cpp
+++ WarCoin-GuerraDeMonedas.cpp
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 main()
 {
 	int CantMinima, CantMaxima;
+	int NumeroAleatorio, Comienza;
 	
 	printf("\nPor favor ingrese la cantidad minima de monedas que se pueden sacar: ");
 	scanf("%d", &CantMinima);
 	printf("\nPor favor ingrese la cantidad maxima de monedas que se pueden sacar: ");
 	scanf("%d", &CantMaxima);
+	
+	srand(time(NULL));	
+	NumeroAleatorio= 10 + rand()%(50-10+1);
+	
+	printf("La cantidad inicial de monedas en la pila es: %d", NumeroAleatorio); 
+	
+	Comienza= 1 + rand()%(2-1+1); 
+	
+	if(Comienza==1)		
+	{
+		printf("\nInicia la partida el usuario"); 	
+	}
+	if(Comienza==2)		
+	{
+		printf("\nInicia la partida la computadora");
+	}
 	
 }
