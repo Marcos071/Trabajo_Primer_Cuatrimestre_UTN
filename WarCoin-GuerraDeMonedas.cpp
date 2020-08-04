e866e0311eea5fab652f86fa415d6d093da2e0dd
Brandan Marcos <Marcos.Brandan@alu.utn.edu.ar>
2020-08-04 14:16:51 -0300

    Agregar mensaje pidiendo al usuario ingresar la minima y la maxima cantidad de monedas que se pueden sacar

---
+++ WarCoin-GuerraDeMonedas.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int CantMinima, CantMaxima;
+	
+	printf("\nPor favor ingrese la cantidad minima de monedas que se pueden sacar: ");
+	scanf("%d", &CantMinima);
+	printf("\nPor favor ingrese la cantidad maxima de monedas que se pueden sacar: ");
+	scanf("%d", &CantMaxima);
+	
+}
